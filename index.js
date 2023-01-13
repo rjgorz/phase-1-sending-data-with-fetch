d7844180ef5d20c552c6a5036de79de4797b6936
Robert Gorz <rjgorz@gmail.com>
2023-01-13 11:53:11 -0600

    functional, submits post request and properly displays on web page, still getting error for first test due to stream argument

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Add your code here
+function submitData(name, email) {
+    return fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify({
+            name,
+            email
+        })
+    })
+    .then(response => response.json())
+    .then(object => {
+        const h1 = document.createElement('h1');
+        h1.textContent = object.id;
+        document.querySelector('body').appendChild(h1);
+    })
+    .catch(error => {
+        const h1 = document.createElement('h1');
+        h1.textContent = error.message;
+        document.querySelector('body').appendChild(h1);
+    });
+}
+
+submitData('Robert', 'robert@gmail.com');
